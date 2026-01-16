c9cac14573bea5552ffb35463f239df9dc6bf6e6
jihyeon <yio1346@gmail.com>
2026-01-16 10:00:42 +0900

    feat: argumentresolver 로그인검증 구현

--- src/main/java/com/backoffice/customer/controller/CustomerController.java
+++ src/main/java/com/backoffice/customer/controller/CustomerController.java
@@ -1,5 +1,7 @@
 package com.backoffice.customer.controller;
 
+import com.backoffice.admin.config.Login;
+import com.backoffice.admin.dto.SessionAdmin;
 import com.backoffice.customer.dto.*;
 import com.backoffice.customer.service.CustomerService;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +25,7 @@ public class CustomerController {
   // 고객 전체 조회
   @GetMapping("/api/customers")
   public ResponseEntity<Page<CustomerGetResponse>> getAll(
+      @Login SessionAdmin sessionAdmin,
       // CustomerGetRequest 정렬조건을 request에 담아서 불러옴
       CustomerGetRequest request,
       @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC)
@@ -32,28 +35,35 @@ public class CustomerController {
 
   // 고객 상세 (단건) 조회
   @GetMapping("/api/customers/{id}")
-  public ResponseEntity<CustomerGetResponse> getById(@PathVariable Long id) {
+  public ResponseEntity<CustomerGetResponse> getById(
+      @Login SessionAdmin sessionAdmin, @PathVariable Long id) {
     return ResponseEntity.status(HttpStatus.OK).body(customerService.findById(id));
   }
 
   // 고객 정보 업데이트
   @PutMapping("/api/customers/{id}")
   public ResponseEntity<CustomerUpdateResponse> update(
-      @PathVariable Long id, @RequestBody CustomerUpdateRequest request) {
+      @Login SessionAdmin sessionAdmin,
+      @PathVariable Long id,
+      @RequestBody CustomerUpdateRequest request) {
     return ResponseEntity.status(HttpStatus.OK).body(customerService.update(id, request));
   }
 
   // 고객 정보 삭제(탈퇴)
   @DeleteMapping("/api/customers/{id}")
   public ResponseEntity<Void> delete(
-      @PathVariable Long id, @RequestBody CustomerDeleteRequest request) {
+      @Login SessionAdmin sessionAdmin,
+      @PathVariable Long id,
+      @RequestBody CustomerDeleteRequest request) {
     return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
   }
 
   // 상태 변경
   @PatchMapping("/api/customers/{id}/status")
   public ResponseEntity<Void> updateStatus(
-      @PathVariable Long id, @RequestBody CustomerStatusUpdateRequest request) {
+      @Login SessionAdmin sessionAdmin,
+      @PathVariable Long id,
+      @RequestBody CustomerStatusUpdateRequest request) {
     return ResponseEntity.status(HttpStatus.OK).build();
   }
 }
