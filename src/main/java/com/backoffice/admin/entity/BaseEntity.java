76f9b532cc3d946cd8a452aae4035594a5bf0dc5
MinWoo1995 <wjsalsdn120@naver.com>
2026-01-16 14:24:27 +0900

    refactor: 튜터님 피드백 반영 - 페이징 최적화, URL 공통화 및 계정 생성 자동화

--- src/main/java/com/backoffice/admin/entity/BaseEntity.java
+++ src/main/java/com/backoffice/admin/entity/BaseEntity.java
@@ -1,7 +1,11 @@
 package com.backoffice.admin.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -11,6 +15,9 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public abstract class BaseEntity {
 
   @CreatedDate
